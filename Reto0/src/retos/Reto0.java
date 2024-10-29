6b4d1bbf220828d489789135cf1d043a1eba76bf
Kristell <krisvm31@gmail.com>
2024-10-29 09:12:52 +0100

    Update Reto0.java

--- Reto0/src/retos/Reto0.java
+++ Reto0/src/retos/Reto0.java
@@ -4,7 +4,7 @@ public class Reto0 {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo222");
-System.out.println("adios mundo222");
+		System.out.println("adios mundo222");
 
 	}
 
